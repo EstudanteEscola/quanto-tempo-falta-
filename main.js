eb08fe1bf8fb97742ddfd413e66b27865a55550a
Ana <107406143+EstudanteEscola@users.noreply.github.com>
2024-05-10 09:24:28 -0300

    saves

---
+++ main.js
@@ -0,0 +1,11 @@
+const dataAtual = new Date ();
+let dataObjetivo = prompt("Data final ANO-MES-DIA
+Exemplo: 2024-04-10");
+
+dataObjetivo = nwe Date (dataObjetivo + "T23:59:59");
+let diasQueFaltam = Math.floor ((dataObjetivo -
+    dataAtual) / 86400000)
+
+    document.querySelector("#dias_restantes").textContent =
+    diasQueFaltam;
+    
